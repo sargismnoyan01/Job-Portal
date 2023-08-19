df5d1344bcc32dfc9843100f58f35b7badf086d1
sargismnoyan01 <128826963+sargismnoyan01@users.noreply.github.com>
2023-08-19 18:41:46 +0400

    Update

--- JobEntry/Job/Job/settings.py
+++ JobEntry/Job/Job/settings.py
@@ -91,8 +91,8 @@ DATABASES = {
 }
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='mnoyansargis@gmail.com'
-EMAIL_HOST_PASSWORD='tdktkappyxjxmmgb'
+EMAIL_HOST_USER='your email'
+EMAIL_HOST_PASSWORD='your password'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
